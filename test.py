ab62a3ae15d35df1496ae5bc7fdc851cd1af323e
Zahil Chuduang <zahil.c@ku.th>
2023-02-18 20:28:01 +0700

    remove temp file

--- test.py
+++
@@ -1,9 +0,0 @@
-places = []
-file_path = "./wiki_data.txt"
-with open(file_path, "r", encoding="utf-8") as f:
-    lines = f.readlines()
-    for line in lines:
-        line = line.strip()
-        places.append(line)
-        
-search_keys = places
