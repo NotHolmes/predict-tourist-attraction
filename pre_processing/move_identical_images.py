3e71de1bbaa68b7a9e3335305d7bd2e14913dfff
Zahil Chuduang <zahil.c@ku.th>
2023-02-23 19:57:32 +0700

    delete -> change identical images{

--- pre_processing/move_identical_images.py
+++ pre_processing/move_identical_images.py
@@ -3,13 +3,18 @@ from PIL import Image
 import imagehash
 
 # directory containing the images
-image_dir = "./The_Grand_Palace_Bangkok"
+place = "wat_phra_chetuphon_(wat_pho)"
+image_dir = "../Google-Image-Scraper/photos/" + place + "/"
 
 # dictionary to store hashes of images
 hashes = {}
 
 # count the number of images before
 num_images_before = len(os.listdir(image_dir))
+
+# create a new directory to store duplicate images
+dup_dir = "./" + "duplicate_images/" + place
+os.makedirs(dup_dir, exist_ok=True)
 
 # iterate over images in the directory
 for filename in os.listdir(image_dir):
@@ -18,14 +23,16 @@ for filename in os.listdir(image_dir):
         # open the image and compute its hash
         img_path = os.path.join(image_dir, filename)
         img = Image.open(img_path)
-        img = img.convert("RGBA")
+        img = img.convert("RGB")
         hash = str(imagehash.phash(img))
         
         # check if hash already exists
         if hash in hashes:
-            # duplicate image found - delete it
-            os.remove(img_path)
-            print(f"Deleted duplicate image {img_path}")
+            # duplicate image found - move it to new directory
+            new_path = os.path.join(dup_dir, filename)
+            os.rename(img_path, new_path)
+            hashes[hash] = new_path
+            print(f"Moved duplicate image {img_path} to {new_path}")
         else:
             # new image - store its hash
             hashes[hash] = img_path
@@ -33,10 +40,10 @@ for filename in os.listdir(image_dir):
 # count the number of images after
 num_images_after = len(os.listdir(image_dir))
 
-# calculate the number of deleted images
-num_deleted_images = num_images_before - num_images_after
+# calculate the number of moved images
+num_moved_images = len(os.listdir(dup_dir))
 
 # print the results
 print(f"Total number of images before: {num_images_before}")
 print(f"Total number of images after: {num_images_after}")
-print(f"Number of deleted images: {num_deleted_images}")
+print(f"Number of moved images: {num_moved_images}")
