6f0342545f5d611c56523217eee2568678969e19
NotHolmes <zahil.c@ku.th>
2023-03-06 22:14:27 +0700

    fix copy pre processing

--- pre_processing/copy_no_overwite.py
+++ pre_processing/copy_no_overwite.py
@@ -1,9 +1,8 @@
 import os
 import shutil
-from datetime import datetime
 
-root_dir = './pre_processing/new/test/'
-backup_dir = './pre_processing/new/testtest/'
+root_dir = './dataset/new/photos/'
+backup_dir = './dataset/data/'
 
 # create the backup directory if it doesn't exist
 if not os.path.exists(backup_dir):
@@ -13,18 +12,17 @@ if not os.path.exists(backup_dir):
 for dir_path, _, file_names in os.walk(root_dir):
     for file_name in file_names:
         # only consider image files
-        if file_name.endswith('.jpg') or file_name.endswith('.png') or file_name.endswith('.jpeg') or file_name.endswith('.webp'):
+        if file_name.endswith('.jpg') or file_name.endswith('.png') or file_name.endswith('.jpeg'):
             # get the file name and extension
             name, extension = os.path.splitext(file_name)
             # add a suffix number to the file name if it already exists
-            suffix = 1
-            new_name = f"{name}_{suffix}{extension}"
             dest_dir = os.path.join(backup_dir, os.path.relpath(dir_path, root_dir))
-            dest_path = os.path.join(dest_dir, new_name)
+            dest_path = os.path.join(dest_dir, file_name)
+            suffix = 1
             while os.path.exists(dest_path):
-                suffix += 1
                 new_name = f"{name}_{suffix}{extension}"
                 dest_path = os.path.join(dest_dir, new_name)
+                suffix += 1
             # create the destination directory if it doesn't exist
             if not os.path.exists(dest_dir):
                 os.makedirs(dest_dir)
